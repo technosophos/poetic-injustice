2dd42e46d073df38c3533983c5d2767772b225da
Matt Butcher <matt.butcher@microsoft.com>
2019-05-30 16:39:23 -0600

    ran cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,15 @@
 extern crate actix_web;
 extern crate rand;
 
+use actix_web::{server, App, HttpRequest, HttpResponse, Responder};
 use rand::prelude::*;
-use actix_web::{
-    server,
-    App,
-    HttpRequest,
-    HttpResponse,
-    Responder,
-};
 use std::env::var as envar;
 
 fn main() {
     server::new(|| {
         App::new()
-        .resource("/", |r| r.f(spout_poetry))
-        .resource("/healthz", |r| r.f(mete_injustice))
+            .resource("/", |r| r.f(spout_poetry))
+            .resource("/healthz", |r| r.f(mete_injustice))
     })
     .bind("0.0.0.0:8000")
     .expect("cannot bind to host")
@@ -32,8 +26,8 @@ const QUOTE: &'static str = r#"
 "#;
 
 fn spout_poetry(_req: &HttpRequest) -> impl Responder {
-    let poet =  envar("POET").unwrap_or(POET.into());
-    let poem =  envar("POEM").unwrap_or(POEM.into());
+    let poet = envar("POET").unwrap_or(POET.into());
+    let poem = envar("POEM").unwrap_or(POEM.into());
     let quote = envar("QUOTE").unwrap_or(QUOTE.into());
 
     format!("{}\n--'{}' by {}\n", quote, poem, poet)
@@ -43,7 +37,7 @@ fn mete_injustice(_req: &HttpRequest) -> HttpResponse {
 
     // Fail about 1% of the time
     if x % 100 == 0 {
-        return HttpResponse::NotFound().body("Oops, I did it again")
+        return HttpResponse::NotFound().body("Oops, I did it again");
     }
     HttpResponse::Ok().body("success")
 }
